0fc1c12723e3a70bbe24c8fb9ca8e4b0b871348f
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-06-11 18:33:30 +0900

    Update profit_calculation.js

--- app/javascript/profit_calculation.js
+++ app/javascript/profit_calculation.js
@@ -5,7 +5,7 @@ item_price.addEventListener("input", () => {
   const add_tax_price = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
   const fee = 0.1;
-  const sales_commission = item_price.value * fee;
+  const sales_commission = Math.floor(item_price.value * fee);
   add_tax_price.innerHTML = sales_commission;
   profit.innerHTML = item_price.value - sales_commission;
 });
