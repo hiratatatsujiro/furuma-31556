a40b7558e5f0605e249a41bacb9b8b5ba5ac0be4
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-06-14 06:07:55 +0900

    rubocopの調整

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     status_id           { Faker::Number.between(from: 2, to: 7) }
     pay_for_shopping_id { Faker::Number.between(from: 2, to: 3) }
     delivery_area_id    { Faker::Number.between(from: 2, to: 48) }
-    day_to_ship_id     { Faker::Number.between(from: 2, to: 4) }
-    price               { Faker::Number.between(from: 300, to: 9_999_999) }
+    day_to_ship_id { Faker::Number.between(from: 2, to: 4) }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
     association :user
   end
 end
