14ede27027a679e8c76bec7a68d78f362a167def
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-06-20 05:39:53 +0900

    rubocopによる調整

--- app/models/purchase_shopping.rb
+++ app/models/purchase_shopping.rb
@@ -1,25 +1,26 @@
 class PurchaseShopping
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :token, :postal_code, :delivery_area_id, :city, :house_number, :building, :phone_number, :purchase_id
+  attr_accessor :user_id, :item_id, :token, :postal_code, :delivery_area_id, :city, :house_number, :building, :phone_number,
+                :purchase_id
 
   with_options presence: true do
     validates :token
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)'} 
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
   end
   validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
   with_options presence: true do
-    validates :city               
-    validates :house_number       
-    validates :phone_number, length: { minimum: 10, message: "is too short" }    
+    validates :city
+    validates :house_number
+    validates :phone_number, length: { minimum: 10, message: 'is too short' }
   end
-  validates :phone_number, length: { maximum: 11, message: "is too long" } 
-  validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input only number'}
-  
+  validates :phone_number, length: { maximum: 11, message: 'is too long' }
+  validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input only number' }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    ShoppingInformation.create(token: token, postal_code: postal_code, delivery_area_id: delivery_area_id, city: city, house_number: house_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
+    ShoppingInformation.create(token: token, postal_code: postal_code, delivery_area_id: delivery_area_id, city: city,
+                               house_number: house_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
--- spec/models/purchase_shopping_spec.rb
+++ spec/models/purchase_shopping_spec.rb
@@ -7,152 +7,150 @@ RSpec.describe PurchaseShopping, type: :model do
     @purchase_shopping = FactoryBot.build(:purchase_shopping, user_id: user.id, item_id: item.id)
     sleep 0.1
   end
-  context "内容に問題がない場合" do
-    it "tokenと住所情報等が適切に入力されていれば登録できる" do
+  context '内容に問題がない場合' do
+    it 'tokenと住所情報等が適切に入力されていれば登録できる' do
       expect(@purchase_shopping).to be_valid
     end
   end
 
-  context "内容に問題がある場合" do
-    it "tokenがなければ保存できない" do
-      @purchase_shopping.token = ""
+  context '内容に問題がある場合' do
+    it 'tokenがなければ保存できない' do
+      @purchase_shopping.token = ''
       @purchase_shopping.valid?
       expect(@purchase_shopping.errors.full_messages).to include("Token can't be blank")
     end
 
-    it "postal_codeがなければ保存できない" do
-      @purchase_shopping.postal_code = ""
+    it 'postal_codeがなければ保存できない' do
+      @purchase_shopping.postal_code = ''
       @purchase_shopping.valid?
       expect(@purchase_shopping.errors.full_messages).to include("Postal code can't be blank")
     end
 
     it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
-      @purchase_shopping.postal_code = "8301211"
+      @purchase_shopping.postal_code = '8301211'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@purchase_shopping.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
     it 'postal_codeが全角数字だと保存できないこと' do
-      @purchase_shopping.postal_code = "８３０１２１１"
+      @purchase_shopping.postal_code = '８３０１２１１'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@purchase_shopping.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
     it 'postal_codeが全角ひらがなだと保存できないこと' do
-      @purchase_shopping.postal_code = "あいう-えおかき"
+      @purchase_shopping.postal_code = 'あいう-えおかき'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@purchase_shopping.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
     it 'postal_codeが全角英字だと保存できないこと' do
-      @purchase_shopping.postal_code = "ｋｋｋ-ｋｋｋｋ"
+      @purchase_shopping.postal_code = 'ｋｋｋ-ｋｋｋｋ'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@purchase_shopping.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
     it 'postal_codeが全角カタカナだと保存できないこと' do
-      @purchase_shopping.postal_code = "アイウ-エオカキ"
+      @purchase_shopping.postal_code = 'アイウ-エオカキ'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@purchase_shopping.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
     it 'postal_codeが半角カタカナだと保存できないこと' do
-      @purchase_shopping.postal_code = "ｱｲｳ-ｴｵｶｷ"
+      @purchase_shopping.postal_code = 'ｱｲｳ-ｴｵｶｷ'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@purchase_shopping.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
     it 'postal_codeが半角英字だと保存できないこと' do
-      @purchase_shopping.postal_code = "aaa-aaaa"
+      @purchase_shopping.postal_code = 'aaa-aaaa'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@purchase_shopping.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
-    it "delivery_area_idが1ならば保存できない" do
+    it 'delivery_area_idが1ならば保存できない' do
       @purchase_shopping.delivery_area_id = 1
       @purchase_shopping.valid?
       expect(@purchase_shopping.errors.full_messages).to include("Delivery area can't be blank")
     end
 
-    it "cityがなければ保存できない" do
-      @purchase_shopping.city = ""
+    it 'cityがなければ保存できない' do
+      @purchase_shopping.city = ''
       @purchase_shopping.valid?
-      #binding.pry
+      # binding.pry
       expect(@purchase_shopping.errors.full_messages).to include("City can't be blank")
     end
 
-    it "house_numberがなければ保存できない" do
-      @purchase_shopping.house_number = ""
+    it 'house_numberがなければ保存できない' do
+      @purchase_shopping.house_number = ''
       @purchase_shopping.valid?
       expect(@purchase_shopping.errors.full_messages).to include("House number can't be blank")
     end
 
-    it "phone_numberがなければ保存できない" do
-      @purchase_shopping.phone_number = ""
+    it 'phone_numberがなければ保存できない' do
+      @purchase_shopping.phone_number = ''
       @purchase_shopping.valid?
       expect(@purchase_shopping.errors.full_messages).to include("Phone number can't be blank")
     end
 
-    it "phone_numberが9字以下ならば保存できない" do
-      @purchase_shopping.phone_number = "090434888"
+    it 'phone_numberが9字以下ならば保存できない' do
+      @purchase_shopping.phone_number = '090434888'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Phone number is too short")
+      expect(@purchase_shopping.errors.full_messages).to include('Phone number is too short')
     end
 
-    it "phone_numberが12字以上であれば保存できない" do
-      @purchase_shopping.phone_number = "09011112222345"
+    it 'phone_numberが12字以上であれば保存できない' do
+      @purchase_shopping.phone_number = '09011112222345'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Phone number is too long")
+      expect(@purchase_shopping.errors.full_messages).to include('Phone number is too long')
     end
 
     it 'phone_numberが全角数字だと保存できないこと' do
-      @purchase_shopping.phone_number = "０９０１１１１２２２２"
+      @purchase_shopping.phone_number = '０９０１１１１２２２２'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Phone number is invalid. Input only number")
+      expect(@purchase_shopping.errors.full_messages).to include('Phone number is invalid. Input only number')
     end
 
     it 'phone_numberが全角ひらがなだと保存できないこと' do
-      @purchase_shopping.phone_number = "あいうえおかきくけこさ"
+      @purchase_shopping.phone_number = 'あいうえおかきくけこさ'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Phone number is invalid. Input only number")
+      expect(@purchase_shopping.errors.full_messages).to include('Phone number is invalid. Input only number')
     end
 
     it 'phone_numberが全角英字だと保存できないこと' do
-      @purchase_shopping.phone_number = "ｋｋｋｋｋｋｋｋｋｋｋ"
+      @purchase_shopping.phone_number = 'ｋｋｋｋｋｋｋｋｋｋｋ'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Phone number is invalid. Input only number")
+      expect(@purchase_shopping.errors.full_messages).to include('Phone number is invalid. Input only number')
     end
 
     it 'phone_numberが全角カタカナだと保存できないこと' do
-      @purchase_shopping.phone_number = "ｱｲｳｴｵｶｷｸｹｺｻ"
+      @purchase_shopping.phone_number = 'ｱｲｳｴｵｶｷｸｹｺｻ'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Phone number is invalid. Input only number")
+      expect(@purchase_shopping.errors.full_messages).to include('Phone number is invalid. Input only number')
     end
 
     it 'phone_numberが半角カタカナだと保存できないこと' do
-      @purchase_shopping.phone_number = "ｱｲｳｴｵｶｷｸｹｺｻ"
+      @purchase_shopping.phone_number = 'ｱｲｳｴｵｶｷｸｹｺｻ'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Phone number is invalid. Input only number")
+      expect(@purchase_shopping.errors.full_messages).to include('Phone number is invalid. Input only number')
     end
 
     it 'phone_numberが半角英字だと保存できないこと' do
-      @purchase_shopping.phone_number = "aaabbbbcccc"
+      @purchase_shopping.phone_number = 'aaabbbbcccc'
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Phone number is invalid. Input only number")
+      expect(@purchase_shopping.errors.full_messages).to include('Phone number is invalid. Input only number')
     end
 
-    it "user_idが空なら保存できない" do
-      @purchase_shopping.user_id = ""
+    it 'user_idが空なら保存できない' do
+      @purchase_shopping.user_id = ''
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("User can't be blank")      
+      expect(@purchase_shopping.errors.full_messages).to include("User can't be blank")
     end
 
-    it "item_idが空ならば保存できない" do
-      @purchase_shopping.item_id = ""
+    it 'item_idが空ならば保存できない' do
+      @purchase_shopping.item_id = ''
       @purchase_shopping.valid?
-      expect(@purchase_shopping.errors.full_messages).to include("Item can't be blank") 
+      expect(@purchase_shopping.errors.full_messages).to include("Item can't be blank")
     end
   end
-
-  
 end
